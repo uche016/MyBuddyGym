fb110342df2f6fe784c8e265b667f26f9ab1e1d4
Uche Onyeator <uonyeator@gmail.com>
2022-12-31 16:52:03 +0000

    added image resource on activity_main added new color on colors.xml made additions on mainactivity

--- app/src/main/java/com/example/mybuddygym/MainActivity.kt
+++ app/src/main/java/com/example/mybuddygym/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mybuddygym
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val startButton: Button = findViewById(R.id.button)
+    val image: Image = findViewById(R.id.imageview)
+
+
 }

61b82c6c2e592c79502cbad822d54f49177477e3
Uche Onyeator <uonyeator@gmail.com>
2023-07-17 17:10:19 +0100

    Made changes to workout details by adding the app user id

--- app/src/main/java/com/example/mybuddygym/database/model/workOutDetails/WorkOutDetails.kt
+++ app/src/main/java/com/example/mybuddygym/database/model/workOutDetails/WorkOutDetails.kt
@@ -1,8 +1,20 @@
 package com.example.mybuddygym.database.model.workOutDetails
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.mybuddygym.database.DatabaseConstants
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 
 
+
+@Entity(tableName = DatabaseConstants.WorkOutDetailsTable.TABLE_NAME)
+data class WorkOutDetails (
+    @SerializedName(DatabaseConstants.WorkOutDetailsTable.ColumnNames.APP_USER_ID)
+    @ColumnInfo(name = DatabaseConstants.WorkOutDetailsTable.ColumnNames.APP_USER_ID )
+    @PrimaryKey
+    @Expose
+    var id: String,
+        )

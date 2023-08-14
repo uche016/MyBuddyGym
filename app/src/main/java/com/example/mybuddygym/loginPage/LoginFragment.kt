1cf41fa9a912541dc962cf31c1593b33aa7b4964
Awwal Oladipupo <awwal.oladipupo@babbangona.com>
2023-08-14 18:22:44 +0100

    updated LoginFragment.kt

--- app/src/main/java/com/example/mybuddygym/loginPage/LoginFragment.kt
+++ app/src/main/java/com/example/mybuddygym/loginPage/LoginFragment.kt
@@ -1,4 +1,41 @@
 package com.example.mybuddygym.loginPage
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mybuddygym.databinding.FragmentLogInPageBinding
+import com.example.mybuddygym.utils.SharedPreferenceMyBuddyApp
+
+class LoginFragment : Fragment() {
+    companion object {
+        private val TAG = this::class.java.simpleName
+    }
+
+    private lateinit var mBinding: FragmentLogInPageBinding
+    private lateinit var mSharedPreference: SharedPreferenceMyBuddyApp
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        mBinding = FragmentLogInPageBinding.inflate(inflater)
+        return mBinding.root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setBinding()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setBinding() {
+        mBinding.btnLogin.setOnClickListener {
+        }
+
+    }
 }
